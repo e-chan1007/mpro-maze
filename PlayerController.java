0ebcc74ee0651219d556cec81a850cbfe83014eb
masaki0710 <masaki0710.prog@gmail.com>
2024-12-15 11:27:29 +0900

    PlayerControllerキー入力受付機構

--- PlayerController.java
+++ PlayerController.java
@@ -1,18 +1,27 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 public class PlayerController implements KeyListener {
+  private PlayerModel playerModel;
+
+  public PlayerController(PlayerModel playerModel) {
+    this.playerModel = playerModel;
+  }
+
   @Override
   public void keyPressed(KeyEvent e) {
     int keyCode = e.getKeyCode();
     if (keyCode == KeyEvent.VK_LEFT) {
-      // Move player lef
-      
+      // Move player left
+      playerModel.moveLeft();
     } else if(keyCode == KeyEvent.VK_RIGHT) {
       // Move player right
+      playerModel.moveRight();
     } else if(keyCode == KeyEvent.VK_UP) {
       // Move player up
+      playerModel.moveUp();
     } else if(keyCode == KeyEvent.VK_DOWN) {
       // Move player down
+      playerModel.moveDown();
     }
   }
 
