f8e9d2181ea63cd21fcda41bf5f11160239c7c6b
masaki0710 <masaki0710.prog@gmail.com>
2025-01-12 22:26:01 +0900

    playerのアニメーション作成

--- app/src/main/java/maze/maze/player/PlayerModel.java
+++ app/src/main/java/maze/maze/player/PlayerModel.java
@@ -10,12 +10,20 @@ public class PlayerModel extends Observable {
     private float playerX = 1;
     private float playerY = 1;
     private boolean keyAcc = true;
+    private boolean isWalkingUp = false;
+    private boolean isWalkingDown = false;
+    private boolean isWalkingLeft = false;
+    private boolean isWalkingRight = false;
     private final int STEPS = 15;
     private final int DELAY = 1000 / 60;
+    private Direction currentDirection;
+
     MazeModel mazeModel = new MazeModel();
 
     public PlayerModel(MazeModel mazeModel) {
         this.mazeModel = mazeModel;
+
+        this.currentDirection = Direction.FOWARD;
 
         this.mazeModel.addObserver((Observable observable, Object object) -> {
             setStartPos();
@@ -44,10 +52,36 @@ public class PlayerModel extends Observable {
         return keyAcc;
     }
 
+    public boolean isWalkingUp() {
+        return isWalkingUp;
+    }
+
+    public boolean isWalkingDown() {
+        return isWalkingDown;
+    }
+
+    public boolean isWalkingLeft() {
+        return isWalkingLeft;
+    }
+
+    public boolean isWalkingRight() {
+        return isWalkingRight;
+    }
+
+    public enum Direction {
+        FOWARD, BACK, LEFT, RIGHT
+    }
+
+    public Direction getCurrentDirection() {
+        return currentDirection;
+    }
+
     // * 連続的な動き試作 */
     public void moveLeft() {
         if (mazeModel.getElementAt(Math.round(playerX - 1), Math.round(playerY)).canEnter()) {
             if (keyAcc) {
+                isWalkingLeft = true;
+                currentDirection = Direction.LEFT;
                 final int[] currentStep = { 0 };
                 keyAcc = false;
                 Timer timer = new Timer(DELAY, e -> {
@@ -59,6 +93,7 @@ public class PlayerModel extends Observable {
                         currentStep[0]++;
                     } else {
                         keyAcc = true;
+                        isWalkingLeft = false;
                         ((Timer) e.getSource()).stop();
                         onMove();
                     }
@@ -71,6 +106,8 @@ public class PlayerModel extends Observable {
     public void moveRight() {
         if (mazeModel.getElementAt(Math.round(playerX + 1), Math.round(playerY)).canEnter()) {
             if (keyAcc) {
+                isWalkingRight = true;
+                currentDirection = Direction.RIGHT;
                 final int[] currentStep = { 0 };
                 keyAcc = false;
                 Timer timer = new Timer(DELAY, e -> {
@@ -82,6 +119,7 @@ public class PlayerModel extends Observable {
                         currentStep[0]++;
                     } else {
                         keyAcc = true;
+                        isWalkingRight = false;
                         ((Timer) e.getSource()).stop();
                         onMove();
                     }
@@ -94,6 +132,8 @@ public class PlayerModel extends Observable {
     public void moveUp() {
         if (mazeModel.getElementAt(Math.round(playerX), Math.round(playerY - 1)).canEnter()) {
             if (keyAcc) {
+                isWalkingUp = true;
+                currentDirection = Direction.FOWARD;
                 final int[] currentStep = { 0 };
                 keyAcc = false;
                 Timer timer = new Timer(DELAY, e -> {
@@ -105,7 +145,9 @@ public class PlayerModel extends Observable {
                         currentStep[0]++;
                     } else {
                         keyAcc = true;
+                        isWalkingUp = false;
                         ((Timer) e.getSource()).stop();
+                        isWalkingUp = false;
                         onMove();
                     }
                 });
@@ -117,6 +159,8 @@ public class PlayerModel extends Observable {
     public void moveDown() {
         if (mazeModel.getElementAt(Math.round(playerX), Math.round(playerY + 1)).canEnter()) {
             if (keyAcc) {
+                isWalkingDown = true;
+                currentDirection = Direction.BACK;
                 final int[] currentStep = { 0 };
                 keyAcc = false;
                 Timer timer = new Timer(DELAY, e -> {
@@ -128,6 +172,7 @@ public class PlayerModel extends Observable {
                         currentStep[0]++;
                     } else {
                         keyAcc = true;
+                        isWalkingDown = false;
                         ((Timer) e.getSource()).stop();
                         onMove();
                     }
--- app/src/main/java/maze/maze/player/PlayerView.java
+++ app/src/main/java/maze/maze/player/PlayerView.java
@@ -11,46 +11,103 @@ import java.util.ArrayList;
 import javax.swing.Timer;
 
 import maze.maze.MazeView;
+import maze.maze.player.PlayerModel.Direction;
 import maze.assets.ImageManager;
 
 public class PlayerView {
   private PlayerModel playerModel;
   private MazeView mazeView;
-  private List<BufferedImage> playerIdleSprite;
+
+  private List<BufferedImage> playerIdleUpSprite;
+  private List<BufferedImage> playerIdleBackSprite;
+  private List<BufferedImage> playerIdleLeftSprite;
+  private List<BufferedImage> playerIdleRightSprite;
+  private List<BufferedImage> playerWalkUp;
+  private List<BufferedImage> playerWalkDown;
+  private List<BufferedImage> playerWalkLeft;
+  private List<BufferedImage> playerWalkRight;
+
   private int currentFrame;
-  private Timer idleAnimeTimer;
+  private Timer AnimationTimer;
 
   public PlayerView(PlayerModel playerModel, MazeView mazeView) {
     this.playerModel = playerModel;
     this.mazeView = mazeView;
 
-    playerIdleSprite = new ArrayList<>();
-    playerIdleSprite.add(ImageManager.PLAYER_IDLE_SPRITE.getImageAt(0, 0));
+    playerIdleUpSprite = new ArrayList<>();
+    playerIdleBackSprite = new ArrayList<>();
+    playerIdleLeftSprite = new ArrayList<>();
+    playerIdleRightSprite = new ArrayList<>();
+    playerWalkUp = new ArrayList<>();
+    playerWalkDown = new ArrayList<>();
+    playerWalkLeft = new ArrayList<>();
+    playerWalkRight = new ArrayList<>();
     for (int i = 0; i < 8; i++) {
-      playerIdleSprite.add(ImageManager.PLAYER_IDLE_SPRITE.getImageAt(i, 0));
+      playerIdleUpSprite.add(ImageManager.PLAYER_IDLE_UP_SPRITE.getImageAt(i, 0));
+      playerIdleBackSprite.add(ImageManager.PLAYER_IDLE_DOWN_SPRITE.getImageAt(i, 0));
+      playerIdleLeftSprite.add(ImageManager.PLAYER_IDLE_LEFT_SPRITE.getImageAt(i, 0));
+      playerIdleRightSprite.add(ImageManager.PLAYER_IDLE_RIGHT_SPRITE.getImageAt(i, 0));
+      playerWalkUp.add(ImageManager.PLAYER_WALKUP_SPRITE.getImageAt(i, 0));
+      playerWalkDown.add(ImageManager.PLAYER_WALKDOWN_SPRITE.getImageAt(i, 0));
+      playerWalkLeft.add(ImageManager.PLAYER_WALKLEFT_SPRITE.getImageAt(i, 0));
+      playerWalkRight.add(ImageManager.PLAYER_WALKRIGHT_SPRITE.getImageAt(i, 0));
     }
 
     this.currentFrame = 0;
 
-    this.idleAnimeTimer = new Timer(1000 / 10, e -> {
+    this.AnimationTimer = new Timer(1000 / 10, e -> {
       if (playerModel.mazeModel.isPaused()) {
         return;
       }
-      currentFrame = (currentFrame + 1) % playerIdleSprite.size();
+      if (playerModel.isIdle() && playerModel.getCurrentDirection() == Direction.FOWARD) {
+        currentFrame = (currentFrame + 1) % playerIdleUpSprite.size();
+      } else if (playerModel.isIdle() && playerModel.getCurrentDirection() == Direction.BACK) {
+        currentFrame = (currentFrame + 1) % playerIdleBackSprite.size();
+      } else if (playerModel.isIdle() && playerModel.getCurrentDirection() == Direction.LEFT) {
+        currentFrame = (currentFrame + 1) % playerIdleLeftSprite.size();
+      } else if (playerModel.isIdle() && playerModel.getCurrentDirection() == Direction.RIGHT) {
+        currentFrame = (currentFrame + 1) % playerIdleRightSprite.size();
+      } else if (playerModel.isWalkingUp()) {
+        currentFrame = (currentFrame + 1) % playerWalkUp.size();
+      } else if (playerModel.isWalkingDown()) {
+        currentFrame = (currentFrame + 1) % playerWalkDown.size();
+      } else if (playerModel.isWalkingLeft()) {
+        currentFrame = (currentFrame + 1) % playerWalkLeft.size();
+      } else if (playerModel.isWalkingRight()) {
+        currentFrame = (currentFrame + 1) % playerWalkRight.size();
+      }
       mazeView.repaint();
     });
-    this.idleAnimeTimer.start();
+    this.AnimationTimer.start();
   }
 
   public void draw(Graphics g) {
     Graphics2D g2d = (Graphics2D) g;
-
     double RATIO = 2.0;
 
-    if (playerModel.isIdle()) {
-      BufferedImage playerIdle = playerIdleSprite.get(currentFrame);
-      int imageWidth = playerIdle.getWidth();
-      int imageHeight = playerIdle.getHeight();
+    BufferedImage playerSprite = null;
+
+    if (playerModel.isIdle() && playerModel.getCurrentDirection() == Direction.FOWARD) {
+      playerSprite = playerIdleUpSprite.get(currentFrame);
+    } else if (playerModel.isIdle() && playerModel.getCurrentDirection() == Direction.BACK) {
+      playerSprite = playerIdleBackSprite.get(currentFrame);
+    } else if (playerModel.isIdle() && playerModel.getCurrentDirection() == Direction.LEFT) {
+      playerSprite = playerIdleLeftSprite.get(currentFrame);
+    } else if (playerModel.isIdle() && playerModel.getCurrentDirection() == Direction.RIGHT) {
+      playerSprite = playerIdleRightSprite.get(currentFrame);
+    } else if (playerModel.isWalkingUp()) {
+      playerSprite = playerWalkUp.get(currentFrame);
+    } else if (playerModel.isWalkingDown()) {
+      playerSprite = playerWalkDown.get(currentFrame);
+    } else if (playerModel.isWalkingLeft()) {
+      playerSprite = playerWalkLeft.get(currentFrame);
+    } else if (playerModel.isWalkingRight()) {
+      playerSprite = playerWalkRight.get(currentFrame);
+    }
+
+    if (playerSprite != null) {
+      int imageWidth = playerSprite.getWidth();
+      int imageHeight = playerSprite.getHeight();
       int cellSize = mazeView.getMazeCellSize();
 
       // * 縦横比維持 */
@@ -65,23 +122,12 @@ public class PlayerView {
       int mazeX = (int) ((playerModel.mazeModel.getMazeWidth() - 0.5f) * mazeView.getMazeCellSize() / 2);
       int mazeY = (int) ((playerModel.mazeModel.getMazeHeight() - 0.5f) * mazeView.getMazeCellSize() / 2);
 
-      g2d.drawImage(playerIdle,
+      g2d.drawImage(playerSprite,
           mazeX + xOffset,
           mazeY + yOffset,
           scaleWidth,
           scaleHeight,
           null);
-    } else {
-      g2d.fill(new Ellipse2D.Float(
-          (playerModel.mazeModel.getMazeWidth() - 0.5f) * mazeView.getMazeCellSize() / 2,
-          (playerModel.mazeModel.getMazeHeight() - 0.5f) * mazeView.getMazeCellSize() / 2,
-          mazeView.getMazeCellSize(),
-          mazeView.getMazeCellSize()));
     }
-    // g2d.fill(new Ellipse2D.Float(playerModel.getPlayerX() *
-    // mazeView.getMazeCellSize(),
-    // playerModel.getPlayerY() * mazeView.getMazeCellSize(),
-    // mazeView.getMazeCellSize(),
-    // mazeView.getMazeCellSize()));
   }
 }
