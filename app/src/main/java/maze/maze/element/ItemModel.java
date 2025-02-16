e7a1556631a7f55ba53fef1ec1bf196ae19e25bc
masaki0710 <masaki0710@gmail.com>
2025-02-16 14:08:30 +0900

    Item系修正

--- app/src/main/java/maze/maze/element/ItemModel.java
+++ app/src/main/java/maze/maze/element/ItemModel.java
@@ -1,10 +1,11 @@
 package maze.maze.element;
 
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 import maze.asset.ImageManager;
 import maze.asset.Sprite;
@@ -16,7 +17,6 @@ import maze.maze.player.PlayerModel;
 
 public class ItemModel extends MazeElement {
     private boolean isItemCollected = false;
-
     private PlayerModel playerModel;
     private List<Item> items;
     private Item selectedItem;
@@ -25,14 +25,20 @@ public class ItemModel extends MazeElement {
 
     private static final Random RANDOM = new Random();
 
+    // HealPotionとSpeedBoostの画像の読み込み
     private static final Sprite HEAL_POTION_SPRITE = ImageManager.loadImageAsSprite("/item/potion.png", 64, 64);
     private static final BufferedImage HEAL_POTION_IMAGE = HEAL_POTION_SPRITE.getImageAt(0, 1);
     private static final Sprite SPEED_BOOST_SPRITE = ImageManager.loadImageAsSprite("/item/potion.png", 64, 64);
     private static final BufferedImage SPEED_BOOST_IMAGE = SPEED_BOOST_SPRITE.getImageAt(0, 4);
 
+    // アイテムボックスの読み込み
     private static final BufferedImage INITIAL_IMAGE = ImageManager.DUNGEON_SPRITE.getImageAt(4, 8);
-    private static final BufferedImage DONE_IMAGE = ImageManager.DUNGEON_SPRITE.getImageAt(5, 8);
 
+    // アイテム名の定数
+    public static final String HEAL_POTION = "Heal Potion";
+    public static final String SPEED_BOOST = "Speed Boost";
+
+    // コンストラクタ
     public ItemModel(MazeModel mazeModel, PlayerModel playerModel) {
         this.playerModel = playerModel;
         this.items = new ArrayList<>();
@@ -49,8 +55,6 @@ public class ItemModel extends MazeElement {
 
     /**
      * アイテムが取得されているかどうかを設定
-     *
-     * @param isCollected アイテムの取得状態
      */
     public void setCollected(boolean isCollected) {
         this.isItemCollected = isCollected;
@@ -58,17 +62,20 @@ public class ItemModel extends MazeElement {
         this.notifyObservers();
     }
 
-    /*
+    /**
      * アイテム名に基づいて画像を返すメソッド
      */
     public static BufferedImage getImage(String itemName) {
         return switch (itemName) {
-            case "Heal Potion" -> HEAL_POTION_IMAGE;
-            case "Speed Boost" -> SPEED_BOOST_IMAGE;
+            case HEAL_POTION -> HEAL_POTION_IMAGE;
+            case SPEED_BOOST -> SPEED_BOOST_IMAGE;
             default -> null;
         };
     }
 
+    /**
+     * 道の画像読み込み
+     */
     @Override
     public void onAllInitiated(MazeModel mazeModel, int x, int y) {
         this.ovalX = x;
@@ -109,10 +116,10 @@ public class ItemModel extends MazeElement {
         }
     }
 
+    // アイテムボックスの描画
     @Override
     public void draw(Graphics g, int x, int y, int size) {
         g.drawImage(pathSprite, x, y, size, size, null);
-
         int itemBoxSize = size / 5 * 4;
         int itemBoxX = x + size / 2 - itemBoxSize / 2;
         int itemBoxY = y + size / 2 - itemBoxSize / 2;
--- app/src/main/java/maze/maze/item/Item.java
+++ app/src/main/java/maze/maze/item/Item.java
@@ -3,9 +3,18 @@ package maze.maze.item;
 import maze.maze.player.PlayerModel;
 
 public interface Item {
+    /**
+     * アイテムの効果を適用するメソッド
+     */
     void applyEffect(PlayerModel playerModel);
 
+    /**
+     * アイテムの画像パスを返すメソッド
+     */
     String getImagePath();
 
+    /**
+     * アイテム名を返すメソッド
+     */
     String getName();
 }
